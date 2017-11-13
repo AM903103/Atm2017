4ca11e3f600402e7977c08ab85da7b86ee67d3d0
Hank Tom <hanktom@gmail.com>
2017-11-13 21:46:00 +0800

    危險權限

--- app/src/main/java/com/litto/atm/ContactActivity.java
+++ app/src/main/java/com/litto/atm/ContactActivity.java
@@ -1,16 +1,45 @@
 package com.litto.atm;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import static android.Manifest.permission.READ_CONTACTS;
 
 public class ContactActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CONTACT = 500;
+    private static final String TAG = ContactActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        if (ActivityCompat.checkSelfPermission(this, READ_CONTACTS)
+                == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{READ_CONTACTS}, REQUEST_CONTACT);
+        }else {
+            readContacts();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CONTACT &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            readContacts();
+        }
+    }
+
+    private void readContacts() {
+        Log.d(TAG, "readContacts: ");
         Cursor cursor = getContentResolver().query(
                 ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
