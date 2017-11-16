f70693d1cf2483d58427016509119b3e8cd1d585
Hank Tom <hanktom@gmail.com>
2017-11-16 22:01:12 +0800

    HistoryActivity/交易記錄

---
+++ app/src/main/java/com/litto/atm/HistoryActivity.java
@@ -0,0 +1,50 @@
+package com.litto.atm;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    private String TAG = HistoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        new HistoryTask().execute("http://atm201605.appspot.com/h");
+    }
+    class HistoryTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                URL url = new URL(strings[0]);
+                InputStream is = url.openStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(is));
+                data = in.readLine();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d(TAG, "onPostExecute: "+s);
+        }
+    }
+}
