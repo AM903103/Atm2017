04575f47ec46bcd06f4518933f1e5b9898dc34f7
Hank Tom <hanktom@gmail.com>
2017-11-23 22:09:44 +0800

    Final

--- app/src/main/java/com/litto/atm/MapsActivity.java
+++ app/src/main/java/com/litto/atm/MapsActivity.java
@@ -38,8 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        mMap.getUiSettings().setZoomControlsEnabled(true);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(25.0339079,121.5623212);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
